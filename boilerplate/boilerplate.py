cc4bae43d037928981063abb6a67411c7bc4a9ca
Shigeru Kitazaki <skitazaki@gmail.com>
2015-04-11 21:37:01 +0900

    Implement progress monitor on boilerplate for Python.

--- boilerplate/boilerplate.py
+++ boilerplate/boilerplate.py
@@ -29,12 +29,18 @@ TODO: Write description on your own.
 
 import argparse
 import configparser
+import csv
+import datetime
+import hashlib
 import json
 import logging
 import logging.config
 import os
+import sqlite3
 import sys
+import time
 import traceback
+from functools import partial
 
 __version__ = '0.1.0'
 __author__ = 'Shigeru Kitazaki'
@@ -102,8 +108,10 @@ def parse_arguments():
     parser.add_argument('-V', '--version', action='version',
                         version='%(prog)s ' + __version__)
 
-    parser.add_argument('-c', '--config', dest='config',
+    parser.add_argument('-c', '--config', dest='config', required=False,
                         help='configuration file', metavar='FILE')
+    parser.add_argument('-m', '--monitor', dest='monitor', required=False,
+                        help='process monitor file', metavar='FILE')
     parser.add_argument('-o', '--output', dest='output',
                         help='output path', metavar='FILE')
     parser.add_argument('-n', '--dryrun', dest='dryrun',
@@ -177,6 +185,16 @@ def collect_files(inputs, recursive=False):
     return files
 
 
+def md5sum(path):
+    # Calculate MD5 sum value.
+    chunk_size = 4096
+    md5 = hashlib.md5()
+    with open(path, 'rb') as fp:
+        for buf in iter(partial(fp.read, chunk_size), b''):
+            md5.update(buf)
+    return md5.hexdigest()
+
+
 class ConfigLoader(object):
     """Configuration file loader to support multiple file types.
 
@@ -228,15 +246,172 @@ class ConfigLoader(object):
         return self._load(ext)
 
 
+class ProgressMonitor(object):
+
+    DEFAULT_MONITOR_FILE = ':memory:'
+    TABLE_NAME = '_monitor'
+    SCHEMA = (
+        {'id': 'seq', 'type': 'integer', 'primary': True},
+        {'id': 'path', 'type': 'string', 'required': True},
+        {'id': 'start_at', 'type': 'float', 'required': True},
+        {'id': 'finish_at', 'type': 'float'},
+        {'id': 'digest', 'type': 'string', 'required': True, 'unique': True},
+        {'id': 'result', 'type': 'string'},  # Anything encoded by JSON
+    )
+
+    def __init__(self, fname):
+        self.logger = logging.getLogger(APPNAME + '.monitor')
+        if fname and os.path.isfile(fname):
+            self.logger.info('Reuse monitor file: %s', fname)
+        self.db = sqlite3.connect(fname or ProgressMonitor.DEFAULT_MONITOR_FILE)
+        self.create_table()
+        self.current = None
+
+    def create_table(self):
+        # Check whether monitor table already exists.
+        r = self.fetch_one('sql',
+                           (('type', '=', 'table'),
+                            ('tbl_name', '=', ProgressMonitor.TABLE_NAME)),
+                           'sqlite_master')
+        if r:
+            self.logger.info('Monitor table is already created.')
+            self.logger.debug(r[0])
+            return
+        d = []
+        # Pattern mapping against JSON Table Schema
+        for s in ProgressMonitor.SCHEMA:
+            t = 'TEXT'
+            if s['type'] == 'integer':
+                t = 'INTEGER'
+            elif s['type'] == 'float':
+                t = 'REAL'
+            f = '{} {}'.format(s['id'], t)
+            if s.get('primary'):
+                f += ' PRIMARY KEY'
+            if s.get('required'):
+                f += ' NOT NULL'
+            if s.get('unique'):
+                f += ' UNIQUE'
+            d.append(f)
+        ddl = """CREATE TABLE {} ({})""".format(
+            ProgressMonitor.TABLE_NAME, ','.join(d))
+        self.logger.debug('Create monitor table: %s', ddl)
+        cur = self.db.cursor()
+        cur.execute(ddl)
+        cur.close()
+        self.logger.info('Created monitor table.')
+
+    def terminate(self, dump=None):
+        self.db.commit()
+        if dump is None:
+            return
+        # Dump monitor records as tab-delimited values.
+        writer = csv.writer(dump, delimiter='\t', lineterminator='\n')
+        header = ('seq', 'path', 'start_at', 'finish_at', 'elapsed', 'digest')
+        writer.writerow(header)
+        q = 'SELECT * FROM {} ORDER BY seq'.format(ProgressMonitor.TABLE_NAME)
+        cur = self.db.cursor()
+        cur.execute(q)
+        for r in cur:
+            t = [r[0], r[1]]
+            start = datetime.datetime.fromtimestamp(r[2])
+            t.append(start.strftime('%Y-%m-%d %H:%M:%S'))
+            if r[3]:
+                finish = datetime.datetime.fromtimestamp(r[3])
+                t.append(finish.strftime('%Y-%m-%d %H:%M:%S'))
+                t.append(r[3] - r[2])
+            else:
+                t.append(None)
+                t.append(None)
+            t.append(r[4])
+            writer.writerow(list(map(lambda s: str(s) if s else '', t)))
+        cur.close()
+
+    def fetch_one(self, columns, conditions, table=None):
+        # It's okay `columns` is string or list.
+        if type(columns) == str:
+            columns = [columns, ]
+        d = []
+        values = []
+        # Column name, Operator, Value tuples.
+        for c, o, v in conditions:
+            # "?" is placement holder.
+            d.append("{} {} ?".format(c, o))
+            values.append(v)
+        q = """SELECT {} FROM {} WHERE {}""".format(
+            ','.join(columns),
+            table or ProgressMonitor.TABLE_NAME,
+            ' AND '.join(d))
+        self.logger.debug('Fetch one record: %s; %s', q, values)
+        cur = self.db.cursor()
+        cur.execute(q, values)
+        r = cur.fetchone()
+        cur.close()
+        return r
+
+    def start(self, path):
+        md5 = md5sum(path)
+        r = self.fetch_one(['seq', 'path', 'start_at', 'finish_at'], (
+            ('digest', '=', md5),
+        ))
+        if r:
+            msg = 'Already processed "{}": [{}] {} -> {}'
+            self.logger.info(msg.format(r[1], r[0], r[2], r[3]))
+            return r
+        self.logger.info('Start monitoring: %s (%s)', path, md5)
+        columns = ('path', 'start_at', 'digest')
+        values = (path, time.time(), md5)
+        q = """INSERT INTO {} ({}) VALUES ({})""".format(
+            ProgressMonitor.TABLE_NAME,
+            ','.join(columns),
+            ','.join(['?' for i in range(len(values))])
+            )
+        self.logger.debug('Insert one record: %s; %s', q, values)
+        cur = self.db.cursor()
+        cur.execute(q, values)
+        cur.close()
+        self.current = md5
+
+    def finish(self, result=None):
+        if self.current is None:
+            self.logger.fatal('Monitor nothing, but `finish()` is called.')
+            return
+        r = self.fetch_one(['seq', 'path', 'start_at'], (
+            ('digest', '=', self.current),
+        ))
+        if r is None:
+            self.logger.fatal('Monitor "%s", but removed.', self.current)
+            return
+        now = time.time()
+        if result:
+            values = (now, json.dumps(result), r[0])
+            columns = ('finish_at = ?', 'result = ?')
+        else:
+            values = (now, r[0])
+            columns = ('finish_at = ?', )
+        q = """UPDATE {} SET {} WHERE seq = ?""".format(
+            ProgressMonitor.TABLE_NAME,
+            ','.join(columns)
+            )
+        self.logger.debug('Update one record: %s; %s', q, values)
+        cur = self.db.cursor()
+        cur.execute(q, values)
+        cur.close()
+        self.current = None
+        self.logger.info('Finish processing: {} [{}] {:,.03f}sec'.format(
+            r[1], r[0], now - r[2]))
+
+
 class MainProcess(object):
 
     """Main process class.
     # TODO: Implement your logic.
     """
 
-    def __init__(self, dryrun):
+    def __init__(self, dryrun, monitor_file=None):
         self.dryrun = dryrun
         self.logger = logging.getLogger(APPNAME + '.main')
+        self.monitor = ProgressMonitor(monitor_file)
 
     def configure(self, configfile):
         loader = ConfigLoader(configfile)
@@ -245,25 +420,38 @@ class MainProcess(object):
             self.logger.debug('config key: %s', k)
         # TODO: Implement your logic.
 
-    def run(self, files, encoding, output):
-        if files:
-            for fname in files:
-                self.logger.info('Start processing: {}'.format(fname))
-                with open(fname, 'r', encoding=encoding) as fp:
-                    _ = self.process(fp, output)
-                self.logger.info('Finish processing: {}'.format(fname))
-        else:
-            _ = self.process(sys.stdin, output)
-
-    def process(self, fp, output):
+    def setup_writer(self, output):
         # TODO: Implement your logic.
         pass
+
+    def run(self, files, encoding):
+        if files:
+            for fname in files:
+                canskip = self.monitor.start(fname)
+                if canskip:
+                    continue
+                with open(fname, 'r', encoding=encoding) as fp:
+                    r = self.process(fp)
+                self.monitor.finish({'lines': r})
+            # TODO: Set output file name for monitor dump.
+            self.monitor.terminate(sys.stdout)
+        else:
+            self.process(sys.stdin)
+
+    def process(self, fp):
+        # TODO: Implement your logic.
+        lines = 0
+        reader = map(str.rstrip, fp)
+        for l in reader:
+            lines += 1
+        return lines
 
 CONFIGURATION = """Start running with following configurations.
 ==============================================================================
   Base directory     : {basedir}
   Current working dir: {cwd}
   Configuration file : {configfile}
+  Monitor file       : {monitorfile}
   Dry-run            : {dryrun}
   Input encoding     : {encoding}
   Input #files       : {nfiles}
@@ -280,7 +468,7 @@ def main():
     args = parse_arguments()
     files = collect_files(args.files, args.recursive)
     encoding = args.encoding
-    processor = MainProcess(args.dryrun)
+    processor = MainProcess(args.dryrun, args.monitor)
     # Check and set configuration file.
     if args.config:
         configfile = args.config
@@ -296,14 +484,15 @@ def main():
         output = open(outputpath, 'w', encoding=args.encoding_out)
     else:
         output = sys.stdout
+    processor.setup_writer(output)
     logger.info(CONFIGURATION.format(basedir=BASEDIR, cwd=os.getcwd(),
                 configfile=args.config, dryrun=args.dryrun,
                 encoding=encoding, nfiles=len(files or []),
-                recursive=args.recursive,
+                recursive=args.recursive, monitorfile=args.monitor,
                 output=args.output, encoding_out=args.encoding_out))
-    # Dispatch main process., and catch unknown error.
+    # Dispatch main process, and catch unknown error.
     try:
-        processor.run(files, encoding, output)
+        processor.run(files, encoding)
     except Exception:
         e = sys.exc_info()[1]
         logger.error(e)
